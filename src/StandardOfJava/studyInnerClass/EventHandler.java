e7cd618162cf7da7a18dc09547c6870c63dd605f
jiyeon12345 <wldys5353@naver.com>
2022-08-19 11:48:04 +0900

    자바의 정석(7장 익명클래스 예제 1)

---
+++ src/StandardOfJava/studyInnerClass/EventHandler.java
@@ -0,0 +1,8 @@
+package StandardOfJava.studyInnerClass;
+import java.awt.event.*;
+
+public class EventHandler implements ActionListener{
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("ActionEvent occurred!");
+	}
+}
---
+++ src/StandardOfJava/studyInnerClass/InnerEx7.java
@@ -0,0 +1,15 @@
+package StandardOfJava.studyInnerClass;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class InnerEx7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Button b = new Button("start");
+		b.addActionListener(new EventHandler());
+	}
+
+}

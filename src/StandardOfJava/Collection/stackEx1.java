8a50a441b3a987cc828525a23503ffa28b900b70
jiyeon12345 <wldys5353@naver.com>
2023-02-19 17:15:12 +0900

    자바의 정석(11장 컬렉션)  stackEx1

---
+++ src/StandardOfJava/Collection/stackEx1.java
@@ -0,0 +1,62 @@
+package StandardOfJava.Collection;
+
+import java.util.*;
+
+public class stackEx1 {
+	//스택 활용 예 : 수식계산. 수식괄호 검사, 워드 undo/redo, 웹브라우저 뒤로 앞으로
+	public static Stack back = new Stack();
+	public static Stack forward = new Stack();
+	
+	public static void main(String[] args) {
+		goUrl("1.NATE");
+		goUrl("2.YAHOO");
+		goUrl("3.NAVER");
+		
+		printStatus();
+		
+		goBack();
+		System.out.println("click back button");
+		printStatus();
+		
+		goBack();
+		System.out.println("click back button");
+		printStatus();
+		
+		goForward();
+		System.out.println("click forward button");
+		printStatus();
+		
+		goUrl("4.DAUM");
+		System.out.println("click new button");
+		printStatus();
+
+	}
+
+	private static void goForward() {
+		if(!forward.empty()) {
+			back.push(forward.pop());
+		}
+	}
+
+	private static void goBack() {
+		if(!back.empty()) {
+			forward.push(back.pop());
+		}
+	}
+
+	private static void printStatus() {
+		// TODO Auto-generated method stub
+		System.out.println("back : " + back);
+		System.out.println("forward : " + forward);
+		System.out.println("now is  " + back.peek());
+		System.out.println();
+	}
+
+	private static void goUrl(String url) {
+		back.push(url);
+		if(!forward.empty()) {
+			forward.clear();
+		}
+	}
+
+}
